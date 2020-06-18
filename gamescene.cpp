f802d72a92f3c3ac35863de1ae8b455fdabfb424
MeruSANN <2529619269@qq.com>
2020-06-18 23:09:34 +0800

    Add struct Type

--- gamescene.cpp
+++ gamescene.cpp
@@ -15,20 +15,6 @@ GameScene::GameScene(QString bgPic)
     setParent(0);
     this->bgPic=bgPic;
     this->setBg(bgPic);
-    QString down = ":/Resource/walls/down.png",
-            downleft = ":/Resource/walls/downleft.png",
-            downright = ":/Resource/walls/downright.png",
-            left = ":/Resource/walls/left.png",
-            leftright = ":/Resource/walls/leftright.png",
-            nodown = ":/Resource/walls/nodown.png",
-            noleft = ":/Resource/walls/noleft.png",
-            noright = ":/Resource/walls/noright.png",
-            noup = ":/Resource/walls/noup.png",
-            right = ":/Resource/walls/right.png",
-            up = ":/Resource/walls/up.png",
-            updown = ":/Resource/walls/updown.png",
-            upleft = ":/Resource/walls/upleft.png",
-            upright = ":/Resource/walls/upright.png";
 
     this->player = new Player(Point(0, 0), ":/Resource/slime3jump.gif");
     this->player->setParent(this);
--- walls.h
+++ walls.h
@@ -2,6 +2,23 @@
 #define WALLS_H
 #include <QString>
 
+struct Type{
+    const QString down = ":/Resource/walls/down.png",
+                  downleft = ":/Resource/walls/downleft.png",
+                  downright = ":/Resource/walls/downright.png",
+                  left = ":/Resource/walls/left.png",
+                  leftright = ":/Resource/walls/leftright.png",
+                  nodown = ":/Resource/walls/nodown.png",
+                  noleft = ":/Resource/walls/noleft.png",
+                  noright = ":/Resource/walls/noright.png",
+                  noup = ":/Resource/walls/noup.png",
+                  right = ":/Resource/walls/right.png",
+                  up = ":/Resource/walls/up.png",
+                  updown = ":/Resource/walls/updown.png",
+                  upleft = ":/Resource/walls/upleft.png",
+                  upright = ":/Resource/walls/upright.png";
+};
+
 class Walls
 {
 public:
